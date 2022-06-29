c92cb9377cae252efbf2cb9d7346337f3189331a
Kirill Kornilov <kirillkornilov1995@mail.ru>
2022-06-29 18:45:32 +0700

    The same calculator with a dynamic library (29 June Homework)

---
+++ calc_with_dynamic_lib_29June/add.c
@@ -0,0 +1,7 @@
+#include "operations.h"
+
+
+int my_add (int arg_1, int arg_2){
+
+	return (arg_1 + arg_2);
+}
---
+++ calc_with_dynamic_lib_29June/sub.c
@@ -0,0 +1,7 @@
+#include "operations.h"
+
+
+int my_sub (int arg_1, int arg_2){
+
+	return (arg_1 - arg_2);
+}
